2dea71cce8f2a38bd84248c15425b2668bc7c459
Reuben Stone <reubenstone@reubens-air.home>
2023-06-22 20:52:19 +0100

    code cleanup

--- src/App/sources.js
+++ src/App/sources.js
@@ -20,15 +20,5 @@ export default [
             'textures/environmentMap/pz.jpg',
             'textures/environmentMap/nz.jpg',
         ]
-    },
-    {
-        name: 'dirtColorTexture',
-        type: 'texture',
-        path: 'textures/dirt/color.jpg'
-    },
-    {
-        name: 'dirtNormalTexture',
-        type: 'texture',
-        path: 'textures/dirt/normal.jpg'
     }
 ]
